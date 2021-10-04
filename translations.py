ef1d87061e8a9d4327a8026add177813b7a90c9f
PrincePatil <90929817+Princepatil96k@users.noreply.github.com>
2021-10-04 17:33:22 +0530

    Update translations.py

--- translations.py
+++ translations.py
@@ -2,11 +2,11 @@ from config import ASSISTANT_NAME
 from helpers.bot_utils import BOT_NAME, USERNAME
 
 
-START_TEXT = f"👋🏻 **Hello**, \n\nThis is **{BOT_NAME}** \nI Can Stream Lives, Radios, YouTube Videos & Telegram Audio / Video Files On Voice Chat Of Telegram Groups. Let's Enjoy Cinematic View Of Group Video Player With Your Friends 😉! \n\n**Made With ❤️ By @ImSafone!** 👑"
+START_TEXT = f"👋🏻 **Hello**, \n\nThis is **{BOT_NAME}** \nI Can Stream Lives, Radios, YouTube Videos & Telegram Audio / Video Files On Voice Chat Of Telegram Groups. Let's Enjoy Cinematic View Of Group Video Player With Your Friends 😉! \n\n**Made With ❤️ By @Princepatil96k!** 👑"
 HELP_TEXT = f"""
 🛠-- **Setting Up Bot**:--
 
-\u2022 Start Voice Chat In Your Group!
+\u2022 Start Voice Chat In Your Group! JOIN @STUDY_FLOWER @STUDY_FLOWER_QUIZ_GROUP
 \u2022 Add Me (@{USERNAME}) & My Assistant (@{ASSISTANT_NAME}) To Your Group!
 \u2022 Give Admin To Me (@{USERNAME}) & My Assistant (@{ASSISTANT_NAME}) In Your Group!
 
@@ -20,3 +20,4 @@ HELP_TEXT = f"""
 \u2022 `/restart` - Restart Bot (Sudo Only)
 """
 ABOUT_TEXT = f"💡-- **Information**:-- \n\nThis bot is created for streaming videos in telegram group video chats using several methods from WebRTC. Powered by pytgcalls the async client API for the Telegram Group Calls and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots. \n\n**This bot licensed under GNU-GPL 3.0 License!**"
+OWNER = @Princepatil96k
